a93714e9b7bccd8bd1b313ef38b7eb719ff3f549
Federico Mestrone <federico@moodsdesign.com>
2022-10-14 19:58:54 +0200

    Enabling TLS in a Spring Boot application

---
+++ nekoshlink/certs/create-server-certs.sh
@@ -0,0 +1,24 @@
+set -x
+
+rm *.csr
+rm *.key
+rm *.p12
+rm *.crt
+rm *.srl
+rm *.jks
+
+# Create the CA certificate
+
+openssl req -x509 -sha512 -days 3650 -newkey rsa:4096 -keyout rootCA.key -out rootCA.crt
+
+# Create the server-side TLS certificate
+
+openssl req -new -newkey rsa:4096 -keyout localhost.key -out localhost.csr
+
+openssl x509 -sha512 -req -CA rootCA.crt -CAkey rootCA.key -in localhost.csr -out localhost.crt -days 365 -CAcreateserial
+
+openssl pkcs12 -export -out localhost.p12 -name "localhost" -inkey localhost.key -in localhost.crt
+
+# Import to Java Key Store for Spring Boot application
+
+keytool -importkeystore -srckeystore localhost.p12 -srcstoretype PKCS12 -destkeystore keystore.jks -deststoretype pkcs12
