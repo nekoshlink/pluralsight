b361c20a042216ba4348412527e356264085829b
Federico Mestrone <federico@moodsdesign.com>
2022-10-14 19:58:55 +0200

    Configuring the HTTP Basic and Form-based challenges

--- nekoshlink/shlink-security/src/main/java/org/nekosoft/shlink/sec/web/ShlinkAdminApiBasicSecurityConfiguration.java
+++ nekoshlink/shlink-security/src/main/java/org/nekosoft/shlink/sec/web/ShlinkAdminApiBasicSecurityConfiguration.java
@@ -1,35 +1,31 @@
 package org.nekosoft.shlink.sec.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-@ConditionalOnWebApplication
-public class ShlinkAdminApiX509SecurityConfiguration {
 
-    @Autowired
-    private UserDetailsService users;
+@Configuration
+@ConditionalOnWebApplication
+public class ShlinkAdminApiBasicSecurityConfiguration {
 
     @Bean
-    @Order(120) // order is important so that more specific matches come before more general ones
-    public SecurityFilterChain filterChainX509AdminApi(HttpSecurity http) throws Exception {
+    @Order(120)
+    public SecurityFilterChain filterChainBasicAdminApi(HttpSecurity http) throws Exception {
         return http
                 .cors()
                 .and()
                 .csrf().disable()
-                .authorizeRequests(authorize ->
+                .authorizeRequests( authorize ->
                         authorize.anyRequest().authenticated()
                 )
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .x509().userDetailsService(users)
+                .httpBasic().realmName("NekoShlink")
                 .and().build();
     }
 
